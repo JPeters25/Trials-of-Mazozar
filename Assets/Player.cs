04190ba20bdcd54064e00bfd916c86c53a502765
Justin Peters <JMarvel525@gmail.com>
2022-09-17 13:27:10 -0700

    add collision detection to player script

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour
 {
     private BoxCollider2D boxCollider;
     private Vector3 moveDelta;
+    private RaycastHit2D hit;
     
     private void Start()
     {
@@ -27,7 +28,22 @@ public class Player : MonoBehaviour
         else if (moveDelta.x < 0)
             transform.localScale = new Vector3(-1, 1, 1);
 
+        // Make sure we can move in this direction by casting a box there first, if the box returns null, we're free to move
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(0, moveDelta.y), Mathf.Abs(moveDelta.y * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if(hit.collider == null)
+        {
         // Make this thing move!
-        transform.Translate(moveDelta * Time.deltaTime);
+        transform.Translate(0, moveDelta.y * Time.deltaTime, 0);
+        }
+
+        hit = Physics2D.BoxCast(transform.position, boxCollider.size, 0, new Vector2(moveDelta.x,0), Mathf.Abs(moveDelta.x * Time.deltaTime), LayerMask.GetMask("Actor", "Blocking"));
+        if (hit.collider == null)
+        {
+            // Make this thing move!
+            transform.Translate(moveDelta.x * Time.deltaTime, 0, 0);
+        }
     }
+
 }
+
