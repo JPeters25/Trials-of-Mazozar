171ffd29c7663790f89eb00182289b998b4e44db
Justin Peters <JMarvel525@gmail.com>
2022-09-17 13:01:14 -0700

    add sprite direction change and movement

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
-
 {
     private BoxCollider2D boxCollider;
-
     private Vector3 moveDelta;
-
+    
     private void Start()
     {
         boxCollider = GetComponent<BoxCollider2D>();
@@ -16,14 +14,20 @@ public class Player : MonoBehaviour
 
     private void FixedUpdate()
     {
-        // Reset moveDelta
-        moveDelta = Vector3.zero;
-
         // Movement input
         float x = Input.GetAxisRaw("Horizontal");
         float y = Input.GetAxisRaw("Vertical");
 
-        Debug.Log(x);
-        Debug.Log(y);
+        // Reset moveDelta
+        moveDelta = new Vector3(x,y,0);
+
+        // Swap sprite direction, whether you're going right or left
+        if (moveDelta.x > 0)
+            transform.localScale = Vector3.one;
+        else if (moveDelta.x < 0)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        // Make this thing move!
+        transform.Translate(moveDelta * Time.deltaTime);
     }
 }
