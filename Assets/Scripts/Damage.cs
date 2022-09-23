27a83d45ae83ba888a3ad1e07e128381b29f7ecf
Justin Peters <JMarvel525@gmail.com>
2022-09-23 15:31:16 -0700

    update logic in weapon.cs

---
+++ Assets/Scripts/Damage.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Damage : MonoBehaviour
+{
+    public Vector3 origin;
+    public int damageAmount;
+    public float pushForce;
+}
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -37,6 +37,26 @@ public class Weapon : Collidable
         }
     }
 
+    protected override void OnCollide(Collider2D coll)
+    {
+        if (coll.tag == "Fighter")
+        {
+            if (coll.name == "Player")
+                return;
+
+            // Create new damage object, then we'll send it to the fighter we've hit
+            Damage dmg = new Damage
+            {
+                damageAmount = damagePoint,
+                origin = transform.position,
+                pushForce = pushForce
+            };
+
+            coll.SendMessage("ReceivedDamage", dmg);
+            
+        }
+    }
+
     private void Swing()
     {
         Debug.Log("Swing");
