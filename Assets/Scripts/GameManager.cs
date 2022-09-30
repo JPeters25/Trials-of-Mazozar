3232c8232de956e30f2028c263f142c2d04bcc65
Justin Peters <JMarvel525@gmail.com>
2022-09-30 14:28:59 -0700

    add menu button to game UI

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+
     private void Awake()
     {
         if (GameManager.instance != null)
