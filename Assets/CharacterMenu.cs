fe4fae01112f4c73433b0066111c3bb0e167f01b
Justin Peters <JMarvel525@gmail.com>
2022-09-30 15:36:51 -0700

    add character selection menu, and stats menu, add character menu script

---
+++ Assets/CharacterMenu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterMenu : MonoBehaviour
+{
+    // Text fields
+    public Text levelText, hitpointText, pesosText, upgradeCostText, xpText;
+
+    // Logic
+    private int currentCharacterSelection = 0;
+    public Image characterSelectionSprite;
+    public Image weaponSprite;
+    public RectTransform xpBar;
+
+
+}
